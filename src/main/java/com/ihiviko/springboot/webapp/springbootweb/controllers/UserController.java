2bf8d39a6bd1f72425488a7d8613d8c87e10a751
ivette <iconstanzav1@gmail.com>
2024-02-16 04:45:57 -0300

    pasar datos a la vista

--- src/main/java/com/ihiviko/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/ihiviko/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,18 +1,30 @@
 package com.ihiviko.springboot.webapp.springbootweb.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
-
     @GetMapping("/details")
-    public String details(){
-        
-    return "details";
+    public String details(Model model) { // primera forma con Model de pasar datos a la vista
+        model.addAttribute("title", "Hola mundo Spring Boot");
+        model.addAttribute("name", "Ivette");
+        model.addAttribute("lastname", "Valdivia Gomez");
 
- }
+        /*
+         * public String details(Map<String, Object> model){ //segunda forma con MAp
+         * forma de pasar datos a la vista
+         * model.put("title","Hola mundo Spring Boot");
+         * model.put("name","Ivette");
+         * model.put("lastname","Valdivia Gomez");
+         */
 
+        return "details";
+
+    }
 
 }
