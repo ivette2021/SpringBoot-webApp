22e778f571b8b02f14fe78be6e3a0a1789818484
ivette <iconstanzav1@gmail.com>
2024-02-17 03:00:41 -0300

    comentarios con posibles personalizaciones de DTO

--- src/main/java/com/ihiviko/springboot/webapp/springbootweb/models/dto/UserDto.java
+++ src/main/java/com/ihiviko/springboot/webapp/springbootweb/models/dto/UserDto.java
@@ -5,8 +5,11 @@ import com.ihiviko.springboot.webapp.springbootweb.models.User;
 public class UserDto { 
 
     private String title ;
-    private User user;
-    
+    private User user;//objeto como una relacion
+    // private String lastname;
+    // private String title ;
+    // private String user; //olvides borrar y llamr los getter and setter
+
     public String getTitle() {
         return title;
     }
