9e7734b6958a621672b273a9a96740f437e9054d
ivette <iconstanzav1@gmail.com>
2024-02-17 02:40:45 -0300

    la clase DTO

--- src/main/java/com/ihiviko/springboot/webapp/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/ihiviko/springboot/webapp/springbootweb/controllers/UserRestController.java
@@ -6,25 +6,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ihiviko.springboot.webapp.springbootweb.models.User;
+import com.ihiviko.springboot.webapp.springbootweb.models.dto.UserDto;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-
 //este esta basado en la clase usercontroller
 
-@RestController //podemos llamar @Controller en ves de restController pero hay que añadir linea 16 @response Body 
-                // esta ultima indica que la respuesta va a ser un json 
+@RestController // podemos llamar @Controller en ves de restController pero hay que añadir linea
+                // 16 @response Body
+                // esta ultima indica que la respuesta va a ser un json
 
-
-@RequestMapping("/api") //para dar ruta por defecto
+@RequestMapping("/api") // para dar ruta por defecto
 public class UserRestController {
 
-    @GetMapping("/details") //rutas son unicas cambiamos details //aqui tambien podemos cambiar a @RequestMapping(path ="/api",method = RequestMethod.GET) 
-    //@ResponseBody
-    public Map<String, Object>details() {  //se cambia porque body es del tipo map
+    @GetMapping("/details")
+
+    //forma con DTO
+    public UserDto details() {
         User user = new User("Ivette", "Valdivia Gomez");
-        Map<String, Object> body = new HashMap<>(); //nuestro map va a ser nuestro JSON
+
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola mundo Spring Boot");
+        
+        return userDto;
+
+    }
+
+    // forma antigua
+    @GetMapping("/details-Map") // rutas son unicas cambiamos details //aqui tambien podemos cambiar a
+                                // @RequestMapping(path ="/api",method = RequestMethod.GET)
+    // @ResponseBody
+    public Map<String, Object> detailsMap() { // se cambia porque body es del tipo map
+        User user = new User("Ivette", "Valdivia Gomez");
+        Map<String, Object> body = new HashMap<>(); // nuestro map va a ser nuestro JSON
 
         body.put("title", "Hola mundo Spring Boot"); // nombre , valor
         body.put("user", user);
